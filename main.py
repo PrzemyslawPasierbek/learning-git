89861d6c4fd604e88429262a9b839c5182478041
Przemysław Pasierbek <przemyslawpasierbek@gmail.com>
2020-07-29 17:05:13 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+country = [("Polska", "Warszawa"), ("Niemcy", "berlin"), ("Czech Republic", "Praha"), ("Slowakei", 15)]
+country_dict = dict(country)
+for capitol in country_dict.items():
+    print(capitol)
