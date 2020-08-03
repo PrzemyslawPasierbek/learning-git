3d9b7f798bda30fd76ad8be58ff560083e0e492f
PrzemyslawPasierbek <49996329+PrzemyslawPasierbek@users.noreply.github.com>
2020-08-03 20:19:16 +0200

    Change language on English

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-country = [("Polska", "Warszawa"), ("Niemcy", "berlin"), ("Czech Republic", "Praha"), ("Slowakei", "bratislava"),("Wegry", "Budapest")]
+country = [("Poland", "Warszawa"), ("Germany", "Berlin"), ("Czech Republic", "Praha"), ("Slowakei", "bratislava"),("Wegry", "Budapest")]
 country_dict = dict(country)
 for capitol in country_dict.items():
     print(capitol)
