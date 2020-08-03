0ef872ba210d188696fb3ad5df4214b7456c67a1
PrzemyslawPasierbek <49996329+PrzemyslawPasierbek@users.noreply.github.com>
2020-08-03 20:20:09 +0200

    Correct the names of capitol

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-country = [("Poland", "Warszawa"), ("Germany", "Berlin"), ("Czech Republic", "Praha"), ("Slowakei", "bratislava"),("Wegry", "Budapest")]
+country = [("Poland", "Warsaw"), ("Germany", "Berlin"), ("Czech Republic", "Praha"), ("Slowakei", "Bratislava"),("Wegry", "Budapest")]
 country_dict = dict(country)
 for capitol in country_dict.items():
     print(capitol)
