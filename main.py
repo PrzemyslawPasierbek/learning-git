d371b0f8b1285b7f8c6042415cfe3a1cae6320b5
PrzemyslawPasierbek <49996329+PrzemyslawPasierbek@users.noreply.github.com>
2020-08-03 20:16:35 +0200

    Add new information about capitol and country

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-country = [("Polska", "Warszawa"), ("Niemcy", "berlin"), ("Czech Republic", "Praha"), ("Slowakei", 15)]
+country = [("Polska", "Warszawa"), ("Niemcy", "berlin"), ("Czech Republic", "Praha"), ("Slowakei", "bratislava"),("Wegry", "Budapest")]
 country_dict = dict(country)
 for capitol in country_dict.items():
     print(capitol)
